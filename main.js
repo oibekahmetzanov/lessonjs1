4738390dec3e7650cd84ab8d2748a043a9f162c7
unknown <oibeka50@gmail.com>
2023-04-28 16:41:16 +0600

    lesson1js

---
+++ main.js
@@ -0,0 +1,25 @@
+const a = prompt('введите число')
+const b = prompt('введите число')
+
+if (a > b) {
+    alert("первое число больше второго");
+}else if (a < b) {
+    alert("второе число больше первого");
+}else if (a === b) {
+    alert("числа равны");
+}
+
+
+// const name = prompt(" укажите ваше имя ")
+// const surname = prompt(" укажите вашу фамилию ")
+// alert( "привет" + name , surname);
+
+
+// const color = prompt("укажите цвет светофора")
+// if(color ==="красный") {
+//     alert("стой")
+// } else if (color === "зеленый") {
+//     alert("иди")
+// } else if (color === "желтый") {
+//     alert("приготовся")
+// }
